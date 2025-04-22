7c01bf5ab50fe615a79ef7e76e81a6c3ce2cdf97
AlTim <aleks00_777@mail.ru>
2025-04-22 17:15:00 +0300

    fix handler.py

--- src/handler.py
+++ src/handler.py
@@ -4,8 +4,12 @@ from typing import Dict, List
 
 
 class AbstractFileHandler(ABC):
+    """Абстрактный класс обработки файла"""
+    def __init__(self, filename):
+        self.__filename = filename
+
     @abstractmethod
-    def add_vacancy(self) -> List[Dict]:
+    def add_vacancy(self, vacancy) -> List[Dict]:
         """Добавить вакансию в файл"""
         pass
 
@@ -25,20 +29,30 @@ class AbstractFileHandler(ABC):
         pass
 
 
-class JSONFileHandler(AbstractFileHandler, ABC):
-    def __init__(self, filename: str = "vacancies.json"):
-        self.vacancy = []
-        self._filename = filename
+class JSONFileHandler(AbstractFileHandler):
+    """Класс обработки файла"""
+    def __init__(self, filename = "vacancies.json"):
+        super().__init__(filename)
 
-    def add_vacancy(self) -> List[Dict]:
+    def add_vacancy(self, vacancy) -> List[Dict]:
         """Добавляем вакансию в JSON-файл"""
+        vacancies = self.get_vacancies()
         try:
-            with open(self._filename, "r", encoding="utf-8") as file:
-                return json.load(file)
+            if vacancy not in vacancies:
+                vacancies.append(vacancy)
+                with open(self._AbstractFileHandler__filename, 'w') as file:
+                    json.dump(vacancies, file, indent=4)
         except FileNotFoundError:
             return []
 
+        # try:
+        #     with open(self.__filename, "r", encoding="utf-8") as file:
+        #         return json.load(file)
+        # except FileNotFoundError:
+        #     return []
+
     def save_data(self, data):
+        """Сохраняем вакансию в файл"""
         try:
             with open("vacancies.json", "r", encoding="utf-8") as file:
                 existing_data = json.load(file)
@@ -58,20 +72,21 @@ class JSONFileHandler(AbstractFileHandler, ABC):
         """Получить вакансии из файла по указанным критериям."""
         filtered_vacancies = []
         try:
-            with open(self._filename, "r", encoding="utf-8") as file:
+            with open(self.__filename, "r", encoding="utf-8") as file:
                 for line in file:
                     if line.strip():  # Проверяем, что строка не пустая
                         data = json.loads(line)
                         if criteria.lower() in data.get("name", "").lower():
                             filtered_vacancies.append(data)
         except FileNotFoundError:
-            print(f"Файл {self._filename} не найден.")
+            print(f"Файл {self.__filename} не найден.")
         except json.JSONDecodeError as e:
             print(f"Ошибка декодирования JSON: {e}")
 
         return filtered_vacancies  # Возвращаем отфильтрованные вакансии
 
     def delete_vacancy(self, vacancy_name):
+        """Удалить вакансии из файла."""
         data = self.add_vacancy()
         # Удаляем вакансию из списка
         data = [vacancy for vacancy in data if vacancy.get("name") != vacancy_name]
