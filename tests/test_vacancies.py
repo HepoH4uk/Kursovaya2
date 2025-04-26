28bdc257a375b9efa0734c3e6c745467d3c1879e
AlTim <aleks00_777@mail.ru>
2025-04-26 16:53:08 +0300

    update test_vacancies.py

--- tests/test_vacancies.py
+++ tests/test_vacancies.py
@@ -1,6 +1,7 @@
 from unittest.mock import Mock, patch
 from src.api import HeadHunterAPI
-
+from src.vacancies import Vacancy
+import pytest
 
 def test_connect_success():
     with patch("requests.get") as mock_get:
@@ -42,3 +43,38 @@ def test_get_vacancies_success():
         assert vacancies[0]["name"] == "Vacancy 1"
         mock_get.assert_called_with(
             api._HeadHunterAPI__base_url, params={"text": "developer", "per_page": 20})
+
+
+def test_vacancy_init():
+    vacancy = Vacancy("1", "Test Vacancy", 1000.0, "http://example.com")
+    assert vacancy.id == "1"
+    assert vacancy.title == "Test Vacancy"
+    assert vacancy.salary == 1000.0
+    assert vacancy.url == "http://example.com"
+
+
+def test_vacancy_salary_setter():
+    vacancy = Vacancy("1", "Test Vacancy", 1000.0, "http://example.com")
+    vacancy.salary = 2000.0
+    assert vacancy.salary == 2000.0
+
+
+def test_vacancy_wrong_salary():
+    vacancy = Vacancy("1", "Test Vacancy", 1000.0, "http://example.com")
+    with pytest.raises(ValueError) as er:
+        vacancy.salary = -2000.0
+    assert str(er.value) == "Заработная плата не может быть отрицательной"
+
+
+def test_vacancy_wrong_id():
+    vacancy = Vacancy("1", "Test Vacancy", 1000.0, "http://example.com")
+    with pytest.raises(ValueError) as er:
+        vacancy.id = None
+    assert str(er.value) == "ID должен быть непустой строкой"
+
+
+def test_vacancy_wrong_title():
+    vacancy = Vacancy("1", "Test Vacancy", 1000.0, "http://example.com")
+    with pytest.raises(ValueError) as er:
+        vacancy.title = None
+    assert str(er.value) == "Название должно быть непустой строкой"
